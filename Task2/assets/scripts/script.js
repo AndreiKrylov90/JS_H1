79558effe9652c762663312b44d2b90ebcc5723c
AndreiKrylov <dronovoy@yandex.ru>
2023-06-08 22:36:46 +0300

    homework was done

---
+++ Task2/assets/scripts/script.js
@@ -0,0 +1,15 @@
+let name = parseInt(prompt("Enter your age"));
+
+switch (name) {
+    case 18:
+        alert('You are an adult, do whatever you want');
+        break;
+    case 10:
+        alert('You have to do your homework');
+        break;
+    case 30:
+        alert('Go to sleep, it is work tomorrow');
+        break;
+    default:
+        alert("I don't know what you can do");
+}
